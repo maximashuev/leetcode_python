3fedfd44a80f2fcc1f48f6cc0202e1db1d9f0354
maximashuev <62572382+maximashuev@users.noreply.github.com>
2024-12-20 09:52:33 -0600

    Update Cheat Sheet.cs

--- C#/Cheat Sheet.cs
+++ C#/Cheat Sheet.cs
@@ -1,6 +1,15 @@
 
 // Create Dictionary
 Dictionary<char, int> my_dict = new Dictionary<char, int>();
+var romans = new Dictionary<char, int>() {
+        ['I'] = 1,
+        ['V'] = 5,
+        ['X'] = 10,
+        ['L'] = 50,
+        ['C'] = 100,
+        ['D'] = 500,
+        ['M'] = 1000
+    };
 
 // Create list 
 List<string> distinctStrings = new List<string>();
